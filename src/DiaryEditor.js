bc7ccd53dc20a08c0bc80b683484c87da13b26e4
kaori-killer <sksbthwjd@daum.net>
2023-03-06 21:31:51 +0900

    feat: 날짜와 동영상 중 하나라도 입력하지 않고 저장한다면, 입력하지 않은 input에 foucs한다

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import App from './App';
 
 const DiaryEditor = () => {
+
+    const dateInput = useRef();
+    const videoInput = useRef();
+
     const [state, setState] = useState({
         date: "2023-03-06",
         video: ""
-    })
+    });
 
     const handleChangeState = (e) => {
         setState({
             ...state, 
             [e.target.name]: e.target.value,
-        })
+        });
     }
 
-    const handleSubmit = () => { console.log("저장 성공")}
+    const handleSubmit = () => { 
+        if(state.date === "") {
+            dateInput.current.focus();
+            return;
+        }
+        if(state.video === "") {
+            videoInput.current.focus();
+            return;
+        }
+
+        console.log("성공");
+    }
 
     return (
         <div className="DiaryEditor">
             <h2>오늘의 1초</h2>
             <div>
-                <input 
+                <input
+                    ref={dateInput}
                     name="date"
                     type="date" 
                     defaultValue={state.date} 
@@ -29,6 +45,7 @@ const DiaryEditor = () => {
             </div>
             <div>
                 <input 
+                    ref={videoInput}
                     name="video"
                     type="file" 
                     accept="video/mp4,video/mkv, video/x-m4v,video/*"
